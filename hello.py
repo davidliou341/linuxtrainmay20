a664455abf133c8aaca293fdc92caa22fac546a0
davidl <david.liou@nih.gov>
2020-05-15 18:05:38 +0000

    Save python

---
+++ hello.py
@@ -0,0 +1,16 @@
+# This is my first python program
+
+name="Raj"
+age=22
+
+brothername="Vijay"
+brotherage=26
+
+agedifference=age-brotherage
+difference=abs(agedifference)
+
+print("My name is: ", name)
+print("My age is: ", age)
+print("My brothers name is: ", brothername)
+print("My brothers age is: ", brotherage)
+print("We were born ", difference, "years apart")
